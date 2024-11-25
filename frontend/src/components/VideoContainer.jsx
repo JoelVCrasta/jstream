0888e1d0ed613934bcc5e8b41987650af2ca208d
Joel Crasta <loejstarc@gmail.com>
2024-11-25 21:44:19 +0530

    added more grids for videocards

--- frontend/src/components/VideoContainer.jsx
+++ frontend/src/components/VideoContainer.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 
 const VideoContainer = ({ children }) => {
-  return <div className="mx-2 my-14 grid grid-cols-5 gap-8">{children}</div>
+  return (
+    <div className="mx-2 my-14 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-8">
+      {children}
+    </div>
+  )
 }
 
 export default VideoContainer
